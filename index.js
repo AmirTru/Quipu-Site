6718029417616ed6ca8a01cbf4fe1d56d6577dcf
AmirTru <amirt49@gmail.com>
2022-08-07 14:07:40 +0300

    first

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ theProcess.find('.dot').on('mouseenter mouseleave', function () {
 });
 
 function moveStart() {
-    $('.process-start').addClass('moving');
+    theProcess.addClass('moving');
 }
 function moveEnd() {
-    $('.process-start').removeClass('moving');
+    theProcess.removeClass('moving');
 }
 
 
