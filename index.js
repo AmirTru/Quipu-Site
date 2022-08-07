90de77d76b9c75a6ed3a808ba064f96e393c365a
AmirTru <amirt49@gmail.com>
2022-08-07 18:08:49 +0300

    first

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ function movingFromHeader() {
 //
 //call on every page render
 $(document).on('mouseenter', '.dot', function () {
-    console.log($(this).index());
+    console.log($(this));
 });
 
 $('.start-your-journey-btn').on('click', function () {
