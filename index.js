7a3e85f004ea9aa500d7ea4fa0870ed8b8feaa46
AmirTru <amirt49@gmail.com>
2022-08-08 20:40:29 +0300

    first

--- index.js
+++ index.js
@@ -47,7 +47,10 @@ $(function () {
         after: function (i, sscroll) {
 
             moveEnd(i);
-        }
+        },
+        afterResize: initialPosition,
+        afterRender: initialPosition
+
     });
 });
 
