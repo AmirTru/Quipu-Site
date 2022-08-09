86862f61affa02a00342803609a179a871d59588
AmirTru <amirt49@gmail.com>
2022-08-09 13:17:21 +0300

    first

--- index.js
+++ index.js
@@ -242,8 +242,9 @@ function setToCenter(getCenter, itemToMove, isRigthSide) {
         itemToMove.offset({ top: centerY - (itemToMove.height() / 2), left: centerX });
     } else {
         var parentWidth = itemToMove.offsetParent().width();
+        console.log(parentWidth)
         var percent = 100 * width / parentWidth;
-        itemToMove.offset({ top: centerY - (itemToMove.height() / 2), left: centerX - itemToMove.width() - (percent * 6) });
+        itemToMove.offset({ top: centerY - (itemToMove.height() / 2), left: centerX - itemToMove.width() - (percent * 10) });
     }
 }
 
