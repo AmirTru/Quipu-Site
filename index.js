6dd0624badccb59c3ee42c3374272ef7abbc15ff
AmirTru <amirt49@gmail.com>
2022-08-11 20:30:41 +0300

    first

--- index.js
+++ index.js
@@ -317,6 +317,7 @@ function researchSectionIn() {
     // theProcess.addClass('research');
     //aling text to dot
     //  theProcess.on('transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd', function (event) {
+        $('.txt').eq(2).addClass('hi');
     setToCenter($('.txt').eq(2), $('.process-text').eq(0), true);
     setToCenter($('.txt').eq(0), $('.process-text').eq(1), true);
     setToCenter($('.txt').eq(1), $('.process-text').eq(2), true);
