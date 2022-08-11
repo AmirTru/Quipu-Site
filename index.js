a8b6f8b6fb927f54487db1592f1582bc8a51560d
AmirTru <amirt49@gmail.com>
2022-08-11 21:58:11 +0300

    first

--- index.js
+++ index.js
@@ -299,7 +299,6 @@ function setToCenter(getCenter, itemToMove, isRigthSide) {
 
     var $this = getCenter;
     var offset = $this.offset();
-    console.log($this.offset());
     var width = $this[0].getBoundingClientRect().width;
     var height = $this[0].getBoundingClientRect().height;
 
