61ecb8cf137d551db5d124c6be845e44c6d0d2e1
AmirTru <amirt49@gmail.com>
2022-08-15 22:09:17 +0300

    first

--- index.js
+++ index.js
@@ -150,8 +150,8 @@ gsap.to("#sphere-the-process", {
     yoyo: true,
     ease: "power1.inOut",
     motionPath: {
-        path: "/62e7de0bdcaac7a20857b639/62fa95cf30a3f2022154684e_svg-path.svg",
-        align: "/62e7de0bdcaac7a20857b639/62fa95cf30a3f2022154684e_svg-path.svg",
+        path: "M 400 600 Q 150 500 250 250 Q 400 0 500 0 Q 800 0 700 500 ",
+        align: "M 400 600 Q 150 500 250 250 Q 400 0 500 0 Q 800 0 700 500 ",
         autoRotate: true,
         alignOrigin: [0.5, 0.5]
     }
